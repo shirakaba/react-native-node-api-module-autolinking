b6a24e07823cf123c2bf91cd600415b4540c45cf
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-02-19 23:10:15 +0900

    migrate to cmake-js

--- packages/example-node-api-module/index.js
+++ packages/example-node-api-module/index.js
@@ -1,6 +1,6 @@
 /** @type {import("./index")} */
 let addon;
 
-addon = require('./build/Release/addon.node');
+addon = require('./build/Release/example-node-api-module.node');
 
 module.exports = addon;
